491a671fd9e87db10db821005593cb9638e18e5d
d-hearts <Friday@Fridays-Wifi-Free.local>
2016-02-17 17:45:50 +0700

    update

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 
 var express    = require("express");
  var mysql      = require('mysql');
+ var bodyParser = require('body-parser')
+
  var connection = mysql.createConnection({
    host     : 'localhost',
    user     : 'root',
-   password : '123456',
+   password : '',
    database : 'frodo'
  });
  var app = express();
+ 
  app.use(express.static(__dirname + '/files'));
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({ extended: true }));
+
  
- 
+
  connection.connect(function(err){
  if(!err) {
      console.log("Database is connected 123 ... \n\n");  
@@ -19,12 +25,28 @@ var express    = require("express");
  }
  });
  
+ 
 app.get('/', function (req, res) {
    console.log("Got a GET request for the homepage");
    res.send('Hello GET');
 })
  
- app.get("/getAllProduct",function(req,res){
+ //Check thông tin Account
+ app.post('/login', function (req, res) {
+  var user_name=req.body.user;
+  var password=req.body.password;
+  var data = {
+        user : user_name,
+        pass : password
+    };
+  
+  console.log("User name = "+user_name+", password is "+password);
+  res.send(data);
+})
+ 
+ 
+// Load tất cả sản phẩm
+app.get("/getAllProduct",function(req,res){
  connection.query('SELECT * from products where active = 1', function(err, rows, fields) {
  // connection.end();
    if (!err)
